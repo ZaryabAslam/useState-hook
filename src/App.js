749b8b2855535955169651f056593307082992b7
ZaryabAslamJatt <66242220+ZaryabAslamJatt@users.noreply.github.com>
2020-10-22 14:13:02 +0500

    usestate hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Parent from './components/Parent';
+
+
+// function App() {
+//   let [counter, setcounter] = useState(0)
+//   let [islit, setlit] = useState(false)
+//   function addition(){
+//     return setcounter = ++counter
+//   }
+//   return (
+//     <div className={`App ${islit? "morning" : "night"}`}>
+//       <h3>The value of counterssss is : {counter}</h3>
+//       <button onClick={addition}>Increment the counter using function</button>
+//       <button onClick={()=> setlit(!islit)}>change light</button>
+//       <button onClick={()=>setcounter(++counter)}>Increment the counter using state</button> 
+//       <h3>The value of counterssss is : {counter}</h3>
+//     </div>
+//   );
+// }
+
+
 
 function App() {
+  let [count, countsetter] = useState(0)
+  function counter(){
+  countsetter(++count)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        the value of count is : {count}
+      </h1>
+      <br />
+      <button onClick={counter}>increment counter using function</button>
+      <button onClick={()=> countsetter(++count)}>increment counter using use state</button>
+     <Parent />
     </div>
   );
 }
---
+++ src/components/Child1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Child1(){
+    return(
+        <div>
+            <h1>this is child1 tag</h1>
+
+        </div>
+    );
+}
+
+export default Child1;
